7b6ff06ea4a8308503d54820a7271e4ceed1e155
Mathias Buus <mathiasbuus@gmail.com>
2011-11-19 16:59:43 +0100

    added a global js file

---
+++ global.js
@@ -0,0 +1,9 @@
+window.template = function (selector, col) {
+	var str = $(selector).html() || $(selector).text();
+
+	col = typeof col === 'object' ? col : Array.prototype.slice.call(arguments, 1);
+
+	return str.replace(/\{([^{}]+)\}/gm, function () {
+		return col[arguments[1]] === undefined ? arguments[0] : col[arguments[1]];
+	});
+};
